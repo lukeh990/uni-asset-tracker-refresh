ec552ff2bc9e56a7c731d21d9f76f73740b63d55
Luke Harding <luke@lukeh990.io>
2025-07-08 23:15:40 -0400

    Baseline Setup

---
+++ cli-tool/src/main.rs
@@ -0,0 +1,10 @@
+/*
+* main.rs
+* Copyright (c) 2025 Luke Harding
+* SPDX License Identifier: MIT
+*/
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
---
+++ firefox-extension/README.md
@@ -0,0 +1,4 @@
+# WIP
+
+I wwnt to make it easier to use the app. So I think I will need to use an
+extension to pull the cookie out of the asset tracker.
